6a81a9a4025e2ece803c8ff05134749b38e6239e
Lux <sophoanreak@gmail.com>
2016-09-12 13:57:31 +0700

    test2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #if Rails.env.production?
-    storage :dropbox
+    storage :file
 #else
    # storage :file
   #end
