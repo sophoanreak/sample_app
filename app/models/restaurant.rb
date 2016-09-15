a5a0ebdacb53d85ffcd5eb95a8973e6853cc031b
Lux <sophoanreak@gmail.com>
2016-09-15 16:13:26 +0700

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	
 	has_many :reviews
+	
+	validates :name, :address, :website, presence: true
+	validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+	message: "Must be in the http:// or https://"
+	}
 end
